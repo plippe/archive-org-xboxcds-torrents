b7c20b79df2c678830d9efe611bb9aaf4fb7a701
Philippe Vinchon <p.vinchon@gmail.com>
2022-11-06 19:10:27 +0000

    Surf get page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+#[async_std::main]
+async fn main() {
     println!("Hello, world!");
+
+    let body = surf::get("https://archive.org/details/xboxcds?&sort=titleSorter&scroll=1&page=1")
+        .recv_string()
+        .await
+        .expect("");
+
+    println!("Body {}", body);
 }
