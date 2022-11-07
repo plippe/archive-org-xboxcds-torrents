cb383db0c9852fdcf51bbdf4f9a2fbb64f9fb3a5
Philippe Vinchon <p.vinchon@gmail.com>
2022-11-07 22:35:28 +0000

    Scrapper parse game links

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod chaining;
 
 use futures::future;
 use futures::stream::{self, Stream, StreamExt};
+use scraper::{Html, Selector};
 
 use crate::chaining::ChainingExt;
 
@@ -28,22 +29,44 @@ async fn get_archive_list_page(page: u64) -> Option<String> {
     }
 }
 
+fn parse_archive_list_page(html: String) -> Vec<String> {
+    let fragment = Html::parse_fragment(&html);
+    let selector = Selector::parse(".item-ttl a").unwrap();
+
+    fragment
+        .select(&selector)
+        .into_iter()
+        .flat_map(|element| element.value().attr("href"))
+        .map(|str| str.to_string())
+        .collect::<Vec<String>>()
+}
+
 fn stream_incremental(init: u64) -> impl Stream<Item = u64> {
     stream::unfold(init, |current| {
         (current, current + 1).pipe(Some).pipe(future::ready)
     })
 }
 
+fn option_to_stream<A>(option: Option<A>) -> impl Stream<Item = A> {
+    option.map_or(vec![], |some| vec![some]).pipe(stream::iter)
+}
+
 #[async_std::main]
 async fn main() {
     println!("Hello, world!");
 
-    let pages = stream_incremental(1)
+    let links = stream_incremental(1)
         .map(get_archive_list_page)
         .buffered(10)
         .take_while(|body| body.is_some().pipe(future::ready))
-        .collect::<Vec<Option<String>>>()
+        .flat_map(option_to_stream)
+        .map(parse_archive_list_page)
+        .flat_map(stream::iter)
+        .collect::<Vec<String>>()
         .await;
 
-    println!("Pages {}", pages.len());
+    println!("Links {}", links.len());
+    println!("- {}", links[0]);
+    println!("- {}", links[1]);
+    println!("- {}", links[2]);
 }
