f10c82da409dc23f3e5ca3ddc3459a811eabfa7b
Philippe Vinchon <p.vinchon@gmail.com>
2023-01-01 21:36:21 +0000

    Get and parse item page

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
-mod chaining;
-
 use futures::future;
 use futures::stream::{self, Stream, StreamExt};
 use scraper::{Html, Selector};
 
-use crate::chaining::ChainingExt;
+const ARCHIVE_HOST: &str = "https://archive.org";
 
-const ARCHIVE_LIST_PAGE_BODY_EMPTY: &str =
+const ARCHIVE_COLLECTION_PAGE_BODY_EMPTY: &str =
     "<div class=\"no-results\">No results matched your criteria.</div>";
 
-async fn get_archive_list_page(page: u64) -> Option<String> {
-    println!("getting page {}", page);
+const ARCHIVE_COLLECTIOM: &str = "xboxcds";
 
+pub fn stream_incremental(init: u64) -> impl Stream<Item = u64> {
+    stream::unfold(init, |current| future::ready(Some((current, current + 1))))
+}
+
+async fn get_archive_collection_page(collection: String, page: u64) -> Option<String> {
     let uri = format!(
-        "https://archive.org/details/xboxcds?&scroll=1&sort=titleSorter&page={}",
-        page
+        "{}/details/{}?&scroll=1&sort=titleSorter&page={}",
+        ARCHIVE_HOST, collection, page
     );
 
-    let body = surf::get(uri)
+    let body = surf::get(&uri)
         .recv_string()
         .await
-        .unwrap_or_else(|_| "".to_owned());
+        .unwrap_or_else(|err| panic!("Failed to get collection page: {}, {}", &uri, err));
 
-    if body == ARCHIVE_LIST_PAGE_BODY_EMPTY {
+    if body == ARCHIVE_COLLECTION_PAGE_BODY_EMPTY {
         None
     } else {
         Some(body)
     }
 }
 
-fn parse_archive_list_page(html: String) -> Vec<String> {
+fn parse_archive_collection_page(html: String) -> Vec<String> {
     let fragment = Html::parse_fragment(&html);
     let selector = Selector::parse(".item-ttl a").unwrap();
 
@@ -37,36 +39,48 @@ fn parse_archive_list_page(html: String) -> Vec<String> {
         .select(&selector)
         .into_iter()
         .flat_map(|element| element.value().attr("href"))
-        .map(|str| str.to_string())
+        .map(|path| format!("{}{}", ARCHIVE_HOST, path))
         .collect::<Vec<String>>()
 }
 
-fn stream_incremental(init: u64) -> impl Stream<Item = u64> {
-    stream::unfold(init, |current| {
-        (current, current + 1).pipe(Some).pipe(future::ready)
-    })
+async fn get_archive_item_page(uri: String) -> String {
+    surf::get(&uri)
+        .recv_string()
+        .await
+        .unwrap_or_else(|err| panic!("Failed to get item page: {}, {}", &uri, err))
 }
 
-fn option_to_stream<A>(option: Option<A>) -> impl Stream<Item = A> {
-    option.map_or(vec![], |some| vec![some]).pipe(stream::iter)
+fn parse_archive_item_page(html: String) -> String {
+    let fragment = Html::parse_fragment(&html);
+    let selector = Selector::parse(".item-download-options .format-group a").unwrap();
+
+    fragment
+        .select(&selector)
+        .into_iter()
+        .filter(|element| element.text().any(|text| text.contains("TORRENT")))
+        .flat_map(|element| element.value().attr("href"))
+        .map(|path| format!("{}{}", ARCHIVE_HOST, path))
+        .collect::<Vec<String>>()
+        .first()
+        .expect("Failed to get item torrent")
+        .to_owned()
 }
 
 #[async_std::main]
 async fn main() {
-    println!("Hello, world!");
-
-    let links = stream_incremental(1)
-        .map(get_archive_list_page)
+    stream_incremental(1)
+        .map(|page| get_archive_collection_page(ARCHIVE_COLLECTIOM.to_owned(), page))
         .buffered(10)
-        .take_while(|body| body.is_some().pipe(future::ready))
-        .flat_map(option_to_stream)
-        .map(parse_archive_list_page)
+        .take_while(|body| future::ready(body.is_some()))
+        .filter_map(future::ready)
+        .map(parse_archive_collection_page)
         .flat_map(stream::iter)
-        .collect::<Vec<String>>()
-        .await;
-
-    println!("Links {}", links.len());
-    println!("- {}", links[0]);
-    println!("- {}", links[1]);
-    println!("- {}", links[2]);
+        .map(get_archive_item_page)
+        .buffered(10)
+        .map(parse_archive_item_page)
+        .for_each(|uri| {
+            println!("{}", uri);
+            future::ready(())
+        })
+        .await
 }
